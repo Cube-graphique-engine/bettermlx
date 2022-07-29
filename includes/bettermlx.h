ce5c790eb4ed6ad531283888ba9e8ccc9f221f04
mathis <mathismartini1@gmail.com>
2022-07-30 01:53:35 +0200

    Add library source code

---
+++ includes/bettermlx.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bettermlx.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mathmart <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/25 19:43:25 by mathmart          #+#    #+#             */
+/*   Updated: 2022/07/04 21:00:33 by mathismartini    ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BETTERMLX_H
+# define BETTERMLX_H
+
+# include "libft.h"
+# include "bettermlx_struct.h"
+# include "bettermlx_func.h"
+# include "mlx.h"
+
+#endif
